c58674115c87ad334651eecf6af15ab03b256324
Michael Kruse <michael.kruse@inria.fr>
2014-01-28 01:53:36 +0100

    merge fixup

--- cmake/modules/TableGen.cmake
+++ cmake/modules/TableGen.cmake
@@ -3,8 +3,12 @@
 # Adds the name of the generated file to TABLEGEN_OUTPUT.
 
 macro(tablegen project ofn)
+message("tablegen(${project} ${ofn} ${ARGN})")
   # Use the list by include_directories().
   get_property(include_dirs DIRECTORY PROPERTY INCLUDE_DIRECTORIES)
+
+  file(GLOB local_tds "*.td")
+  file(GLOB_RECURSE global_tds "${LLVM_MAIN_INCLUDE_DIR}/llvm/*.td")
 
   # Collect possible dependent *.td(s).
   # FIXME: It is far from optimal.
@@ -25,7 +29,7 @@ macro(tablegen project ofn)
   foreach(inc ${include_dirs})
     list(APPEND TABLEGEN_INCLUDE_DIRECTORIES -I ${inc})
   endforeach()
-  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ofn}.tmp
+  add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${ofn}.tmp"
     # Generate tablegen output in a temporary file.
     COMMAND ${${project}_TABLEGEN_EXE} ${EX_DEFAULT_ARGS} -I ${CMAKE_CURRENT_SOURCE_DIR}
     -I ${CMAKE_CURRENT_SOURCE_DIR} -I ${LLVM_MAIN_SRC_DIR}/lib/Target -I ${LLVM_MAIN_INCLUDE_DIR}
@@ -35,11 +39,11 @@ macro(tablegen project ofn)
     # The file in LLVM_TARGET_DEFINITIONS may be not in the current
     # directory and local_tds may not contain it, so we must
     # explicitly list it here:
-    DEPENDS ${${project}_TABLEGEN_EXE} ${dependent_tds} ${EX_DEPENDS}
+    DEPENDS ${${project}_TABLEGEN_EXE} ${dependent_tds} ${local_tds} ${global_tds} ${EX_DEPENDS}
     ${LLVM_TARGET_DEFINITIONS_ABSOLUTE}
     COMMENT "Building ${ofn}..."
     )
-  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ofn}
+  add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${ofn}"
     # Only update the real output file if there are any differences.
     # This prevents recompilation of all the files depending on it if there
     # aren't any.
@@ -53,8 +57,11 @@ macro(tablegen project ofn)
   # `make clean' must remove all those generated files:
   set_property(DIRECTORY APPEND
     PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${ofn}.tmp ${ofn})
-
-  set(TABLEGEN_OUTPUT ${TABLEGEN_OUTPUT} ${CMAKE_CURRENT_BINARY_DIR}/${ofn} PARENT_SCOPE)
+    
+#message("TABLEGEN_OUTPUT=${TABLEGEN_OUTPUT} + ${CMAKE_CURRENT_BINARY_DIR}/${ofn}")
+  list(APPEND TABLEGEN_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${ofn}")
+  #set(TABLEGEN_OUTPUT ${TABLEGEN_OUTPUT} ${CMAKE_CURRENT_BINARY_DIR}/${ofn} PARENT_SCOPE)
+#message("TABLEGEN_OUTPUT=${TABLEGEN_OUTPUT}")
   set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${ofn} 
     PROPERTIES GENERATED 1)
   set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${ofn} PROPERTIES HEADER_FILE_ONLY ON)
@@ -62,10 +69,12 @@ macro(tablegen project ofn)
 endmacro(tablegen)
 
 macro(add_public_tablegen_target target)
+message("macro(add_public_tablegen_target ${target}) TABLEGEN_OUTPUT=${TABLEGEN_OUTPUT}")
   # Creates a target for publicly exporting tablegen dependencies.
   if( TABLEGEN_OUTPUT )
     add_custom_target(${target}
       DEPENDS ${TABLEGEN_OUTPUT})
+message("add_custom_target(${target} DEPENDS ${TABLEGEN_OUTPUT})")
     if (LLVM_COMMON_DEPENDS)
       add_dependencies(${target} ${LLVM_COMMON_DEPENDS})
     endif ()
